24ff0dcaffe95f293cc734cba728e5980d96a3d9
ynetesov <yuriy.netesov@gmail.com>
2018-12-05 09:36:41 +0100

    subset of side effects

--- src/main/scala/space/dehun/BetterThanIoMain.scala
+++ src/main/scala/space/dehun/BetterThanIoMain.scala
@@ -13,26 +13,39 @@ object BetterThanIoMain extends App {
   type Level0[A] = EitherK[LogAction, NetAction, A]
   type AppStack[A] = EitherK[DbAction, Level0, A]
 
-  def foo(x:Int) = for {
-    user <- Db.queryUser(x.toString).inject[AppStack]
-    _ <- Log.logMsg(s"got user ${user}").inject[AppStack]
-    _ <- Db.storeUser(user.copy(age=user.age + 1)).inject[AppStack]
+  type LogAndDb[A] = EitherK[LogAction, DbAction, A]
+  def foo(x:Int):Free[LogAndDb, User] = for {
+    user <- Db.queryUser(x.toString).inject[LogAndDb]
+    _ <- Log.logMsg(s"got user ${user}").inject[LogAndDb]
+    _ <- Db.storeUser(user.copy(age=user.age + 1)).inject[LogAndDb]
   } yield user
 
-  def bar(user:User) = for {
-    _ <- Log.logMsg("lets notify user change!").inject[AppStack]
-    _ <- Net.notifyUserChange(user).inject[AppStack]
+  type LogAndNet[A] = EitherK[LogAction, NetAction, A]
+  def bar(user:User):Free[LogAndNet, Int] = for {
+    _ <- Log.logMsg("lets notify user change!").inject[LogAndNet]
+    _ <- Net.notifyUserChange(user).inject[LogAndNet]
   } yield user.age
 
   override def main(args: Array[String]): Unit = {
-    val logIoNat = new LogIoNat()
-    val dbIoNat = new DbIoNat()
-    val netIoNat = new NetIoNat()
+    lazy val logIoNat = new LogIoNat()
+    lazy val dbIoNat = new DbIoNat()
+    lazy val netIoNat = new NetIoNat()
 
-    val level0Nat = logIoNat.or(netIoNat)
-    val appStackNat = dbIoNat.or[Level0](level0Nat)
+    lazy val level0Nat = logIoNat.or(netIoNat)
+    lazy val appStackNat = dbIoNat.or[Level0](level0Nat)
 
-    val r = (foo(12) flatMap bar)
+    implicit val logAndDbIntoAppStack = new InjectK[LogAndDb, AppStack] {
+      override def inj: ~>[LogAndDb, AppStack] = new ~>[LogAndDb, AppStack] {
+        override def apply[A](fa: LogAndDb[A]): AppStack[A] = fa match {
+          case EitherK(Left(act)) => InjectK[LogAction, AppStack].inj(act)
+          case EitherK(Right(act)) => InjectK[DbAction, AppStack].inj(act)
+        }
+      }
+
+      override def prj: ~>[AppStack, λ[α => Option[LogAndDb[α]]]] = ??? // not used
+    }
+
+    val r = (foo(12).inject[AppStack] flatMap (u => bar(u).inject[AppStack]))
       .foldMap(appStackNat)
       .unsafeRunSync()
     Console.println(s"end result: $r")
